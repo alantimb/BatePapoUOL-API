0e32b5f21f7d26d16658d0ee169c1a924013c3e5
Alan Timbó <alancorreiatimbo@gmail.com>
2023-01-16 11:25:13 -0300

    post request '/messages' data validation and connection with data base

--- src/app.js
+++ src/app.js
@@ -30,9 +30,7 @@ app.post("/participants", async (req, res) => {
   const validation = userSchema.validate(name);
 
   if (validation.error) {
-    res
-      .status(422)
-      .send("Nome em formato inválido!\nPor favor, tente novamente.");
+    res.status(422).send(validation.error.details);
   }
 
   try {
@@ -67,6 +65,48 @@ app.get("/participants", async (req, res) => {
     const resp = await participantsCollection.find().toArray();
 
     return res.status(201).send(resp);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const from = req.headers.user;
+  const timeNow = dayjs().format("HH:mm:ss");
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required(),
+    from: joi.string().required(),
+  });
+  const validation = messageSchema.validate(
+    { to: to, text: text, type: type },
+    { abortEarly: false }
+  );
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+  }
+
+  const participantOn = await participantsCollection.findOne({ name: from });
+
+  if (!participantOn) {
+    res.sendStatus(422);
+  }
+
+  try {
+    await messagesCollection.insertOne({
+      from: from,
+      to: to,
+      text: text,
+      type: type,
+      time: timeNow,
+    });
+
+    return res.sendStatus(201);
   } catch (err) {
     return res.status(500).send(err.message);
   }
